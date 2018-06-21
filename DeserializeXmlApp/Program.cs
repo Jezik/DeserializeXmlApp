4f2191c7df4fc567f469f4c8c8f279e8bc3cc689
Dmitry Baev <dimon1601@yandex.ru>
2018-06-21 10:03:45 +0300

    Reading directory from string[] args

--- DeserializeXmlApp/Program.cs
+++ DeserializeXmlApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace DeserializeXmlApp
     {
         static void Main(string[] args)
         {
+            string[] filesArray = null;
+
+            // Reading directory from arguments
+            try
+            {                
+                filesArray = Directory.GetFiles(args[0], "*.xml");
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Path is a filename");
+                Console.WriteLine(e.StackTrace);
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Access denied");
+                Console.WriteLine(e.StackTrace);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("Wrong path");
+                Console.WriteLine(e.StackTrace);
+            }            
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.WriteLine(filesArray.Length);
+            Console.ReadKey();
         }
     }
 }
